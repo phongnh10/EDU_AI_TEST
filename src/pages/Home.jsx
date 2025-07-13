c8ab0bc12e685a13243509409e0fad2f913d173a
Phong Nguyen <nguyenhongphong1010.02@gmail.com>
2025-07-13 23:18:54 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import CategoryList from "../components/categories/CategoryList";
 import SearchBox from "../components/search/SearchProducts";
 import { useEffect, useState } from "react";
 import PriceRangeFilter from "../components/products/PriceRangeFilter";
-import { useCategories, useProducts } from "../hooks/appHook";
+import { useProducts, useCategories } from "../hooks/appHook";
 
 function Home() {
   const { products, filterProducts } = useProducts();
